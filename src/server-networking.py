639813fa8132ea14fc18b2800d3bda4b09fdcdd2
mattlm0831 <32942111+mattlm0831@users.noreply.github.com>
2019-12-04 13:41:36 -0500

    Added query

--- src/server-networking.py
+++ src/server-networking.py
@@ -1,12 +1,26 @@
-import paramiko
+from fabric import Connection
 
+valid_arguments = ['namespace', 'collection', 'is_member_of','content_model','without_cmodel','with_dsid','without_dsid','solr_query']
 
+class ssh:
 
-class ssh_connection:
-
-    def __init__(self, server, user, password):
-        self.ssh = paramiko.SSHClient()
-        self.ssh.connect(server = server, user = user, password = password)
+    def __init__(self, user, host, password):
+        self.ssh = Connection(host= user + '@' + host, connect_kwargs={"password": password})
+        
+    def __del__(self):
+        self.ssh.close()
     
-    def ls(self):
-        return self.ssh.exec_command('ls')
+    def query(self, **kwargs):
+        with self.ssh.cd("/var/www/drupal7/"):
+            if len(kwargs) < 1:
+                return
+            for k,v in kwargs.items():
+                if k.lower() not in valid_arguments:
+                    raise(ValueError)
+            arguments = ' '.join(['--' + str(k) + '=' + str(v) for k,v in kwargs.items()])
+            
+            cmd = 'sudo drush islandora_datastream_crud_fetch_pids ' + arguments + ' --pid_file=/var/www/drupal7/tmp/pids.txt'
+            self.ssh.run(cmd)
+            self.ssh.get("/var/www/drupal7/tmp/pids.txt", 'pids.txt')
+            
+            
